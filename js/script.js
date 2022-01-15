c97c4971ca6771c7bb5705260fb5c8177d941b3d
shuvomh2000 <shuvomh2000@gmail.com>
2022-01-15 21:32:37 +0600

    3/2done

--- js/script.js
+++ js/script.js
@@ -1,57 +1,43 @@
-
-// for hour
-let count1 = document.querySelector(".count1")
-let countnum1 = 0
+// let count1 = document.querySelector(".count1")
+// let countnum1 = 0
 
 
-function counter1() {
-    countnum1++
-    count1.innerHTML = countnum1
+// function counter1() {
+//     countnum1++
+//     count1.innerHTML = countnum1
 
-    if(countnum1 == count1.dataset.number){
-        clearInterval(stop1)
-    }
-}
-
-let stop1 = setInterval(function(){
-    counter1()
-},100)
-
-
-
-
-// for min
-let count2 = document.querySelector(".count2")
-let countnum2 = 0
-
-
-
-function counter2() {
-    countnum2++
-    count2.innerHTML = countnum2
-
-    if(countnum2 == count2.dataset.number){
-        clearInterval(stop2)
-    }
-}
-
-let stop2 = setInterval(function(){
-    counter2()
-},100)
-//  for sec
-// let count3 = document.querySelector(".count3")
-// let countnum3 = 0
-
-
-// function counter3() {
-//     countnum3++
-//     count3.innerHTML = countnum3
-
-//     if(countnum3 == count3.dataset.number){
-//         clearInterval(stop3)
+//     if(countnum1 == count1.dataset.number){
+//         clearInterval(stop1)
 //     }
 // }
 
-// let stop3 = setInterval(function(){
-//     counter3()
-// },1000)
+// let stop1 = setInterval(function(){
+//     counter1()
+// },100)
+
+
+
+let forcountarray = document.querySelectorAll(".count")
+let arr = Array.from(forcountarray)
+let box = document.querySelector(".box")
+// let forboxarray = document.querySelectorAll(".box") 
+// let box = Array.from(forboxarray)
+
+arr.map(function (item) {
+    let count = 0
+
+    function counter() {
+        count++
+        item.innerHTML = count
+
+        if (count == item.dataset.number) {
+            clearInterval(stop)
+            item.style.background = "green"
+        }
+    }
+    let stop = setInterval(function () {
+        counter()
+    }, 100)
+
+
+})
